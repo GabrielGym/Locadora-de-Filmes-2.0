5f7ac4949da8c828482cfd0006f9de18740c6c06
GabrielGym <biel.ferreira12342@gmail.com>
2023-04-30 22:04:07 -0300

    movies

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import 'reflect-metadata'
+import express, { Application } from "express";
+
+const app: Application = express()
+app.use(express.json())
+
+export default app
--- src/entities/movies.entity.ts
+++ src/entities/movies.entity.ts
@@ -1,3 +1,21 @@
-class Movie {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("movies")
+class Movie {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @Column({ type: "varchar", length: 50, unique: true })
+  name: string
+
+  @Column({ type: "text", nullable: true })
+  description?: string | null 
+
+  @Column({ type: "integer" })
+  duration: number
+
+  @Column({ type: "integer" })
+  price: number
+}
 
 export default Movie;
